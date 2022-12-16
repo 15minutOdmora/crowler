582d181038691d075571b269fe1aaa197c2422e5
15minutOdmora <liammislej@gmail.com>
2022-12-16 21:34:49 +0100

    Add argument parsing and browser options.

--- crowler/__main__.py
+++ crowler/__main__.py
@@ -1,10 +1,24 @@
-
-
+import argparse
 from IPython import embed
 
-from crowler.driver import ChromeDriver
+from crowler.driver import AvailableBrowserDrivers
 
+parser = argparse.ArgumentParser(
+    prog = 'Crowler',
+    description = 'A Selenium based web crowling',
+    prefix_chars='-+'
+)
 
-driver = ChromeDriver()
+parser.add_argument("-b", "--browser", default="chrome")
+
+args = parser.parse_args()
+
+browser_name = args.browser
+
+if browser_name not in AvailableBrowserDrivers:
+    print(f"Could not find the browser {args.browser}, running Chrome.")
+    browser_name = "chrome"
+
+driver = AvailableBrowserDrivers[browser_name]()
 
 embed()
